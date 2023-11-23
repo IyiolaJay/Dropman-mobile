7f29c75f1d07a6fbe47a70ed42a7f0e4e640f4e2
IyiolaJay <jimoh.iyiola11@gmail.com>
2023-11-23 12:51:17 +0100

    Completed splash screen

--- lib/controller/onboarding/splash_screen.dart
+++ lib/controller/onboarding/splash_screen.dart
@@ -1,5 +1,8 @@
-import 'package:dropman/styles/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../styles/colors.dart';
+
 
 class SplashScreenItem extends StatefulWidget {
   const SplashScreenItem({super.key});
@@ -8,22 +11,63 @@ class SplashScreenItem extends StatefulWidget {
   State<SplashScreenItem> createState() => _SplashScreenItemState();
 }
 
-class _SplashScreenItemState extends State<SplashScreenItem> {
-  
+class _SplashScreenItemState extends State<SplashScreenItem>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
   @override
   void initState() {
-    Future.delayed(
-      const Duration(seconds: 3), () {
-      //push to home page or onboarding..
-    });
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    _animation = Tween<double>(begin: 10, end: 15).animate(_controller)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          _controller.forward();
+        }
+      });
+
+    _controller.forward();
+
+    
+       
     super.initState();
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
-      backgroundColor: kPrimaryColorWhite,
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SvgPicture.asset(
+          "assets/images/dropman_letter.svg",
+          fit: BoxFit.cover,
+          height: 100,
+          width: 100,
+        ),
+        AnimatedBuilder(
+          animation: _animation,
+          builder: (context, child) {
+            return CircleAvatar(
+              backgroundColor: kPrimaryColorWhite,
+              radius: _animation.value,
+            );
+          },
+        ),
+      ],
     );
   }
-  
 }
--- lib/view/onboarding/splash_screen.dart
+++ lib/view/onboarding/splash_screen.dart
@@ -3,12 +3,47 @@ import 'package:flutter/services.dart';
 
 import '../../controller/onboarding/splash_screen.dart';
 import '../../styles/colors.dart';
+import 'package:dropman/view/onboarding/welcome_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
+
+  PageRouteBuilder pushHomePage (context) {
+      return PageRouteBuilder(
+            pageBuilder: (context, animation, secondaryAnimation) =>
+                const WelcomeScreen(),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+              const begin = Offset(1.0, 0.0);
+              const end = Offset.zero;
+              const curve = Curves.easeInOut;
+
+              final tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+              final offsetAnimation = animation.drive(tween);
+
+              return SlideTransition(
+                position: offsetAnimation,
+                child: child,
+              );
+            },
+
+            transitionDuration: const Duration(seconds: 1),
+          );
+    }
+
   @override
   Widget build(BuildContext context) {
+    
+    Future.delayed(
+      const Duration(seconds: 4),
+      () {
+        Navigator.pushReplacement(
+          context, pushHomePage(context)
+          
+        );
+      },
+    );
 
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
       statusBarIconBrightness: Brightness.light,
