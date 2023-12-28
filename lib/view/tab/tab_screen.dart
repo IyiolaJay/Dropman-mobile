65d2be60e6f97b5e38d0e9b9ab00db5e7894b73a
IyiolaJay <jimoh.iyiola11@gmail.com>
2023-12-29 00:38:15 +0100

    Added Navigation bar screens placeholder

--- lib/view/tab/tab_screen.dart
+++ lib/view/tab/tab_screen.dart
@@ -1,49 +1,72 @@
 import 'dart:ui';
-import 'package:dropman/view/dashboard/dashboard_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:dropman/view/dashboard/dashboard_screen.dart';
+import 'package:dropman/view/settings/settings_screen.dart';
 
-class TabScreen extends StatelessWidget {
+import '../request/request_screen.dart';
+import '../tracking/tracking_screen.dart';
+
+class TabScreen extends StatefulWidget {
   const TabScreen({super.key});
 
   @override
+  State<TabScreen> createState() => _TabScreenState();
+}
+
+class _TabScreenState extends State<TabScreen> {
+  var _currentIndex = 0;
+
+  final List<BottomNavigationBarItem> navBarList = const [
+    BottomNavigationBarItem(
+      icon: Icon(
+        Icons.home_filled,
+      ),
+      label: "Home",
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(
+        Icons.track_changes_rounded,
+      ),
+      label: "Track",
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(
+        Icons.send,
+      ),
+      label: "Request",
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(
+        Icons.settings,
+      ),
+      label: "Settings",
+    ),
+  ];
+
+  final List<Widget> navPages = [
+    const DashboardScreen(),
+    const TrackingScreen(),
+    const RequestScreen(),
+    const SettingsScreen(),
+  ];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: const DashboardScreen(),
-      bottomNavigationBar: ClipRect(
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 15,sigmaY: 15),
-          child: SizedBox(
-            height: 80,
+      body: navPages[_currentIndex],
+      bottomNavigationBar: SizedBox(
+        height: 80,
+        child: ClipRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
             child: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              
-              icon: Icon(
-                Icons.home_filled,
-              ),
-              label: "Home",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.track_changes_rounded,
-              ),
-              label: "Track",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.send,
-              ),
-              label: "Request",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.settings,
-              ),
-              label: "Settings",
-            ),
-              ]),
+                currentIndex: _currentIndex,
+                onTap: (value) {
+                  setState(() {
+                    _currentIndex = value;
+                  });
+                },
+                type: BottomNavigationBarType.fixed,
+                items: <BottomNavigationBarItem>[...navBarList]),
           ),
         ),
       ),
